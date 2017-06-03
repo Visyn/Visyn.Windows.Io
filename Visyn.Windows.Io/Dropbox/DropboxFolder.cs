0fdd3a531e71a772cea19cc8dd0bd8e686edb1b2
randlee <github@randlee.com>
2017-06-03 12:59:40 -0700

    Dropbox class added to windows IO Random UInt32 Int64 and UInt64 updated

---
+++ Visyn.Windows.Io/Dropbox/DropboxFolder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Visyn.Windows.Io.Dropbox
+{
+    class DropboxFolder
+    {
+        public string Directory ()
+        {
+            var infoPath = @"Dropbox\info.json";
+
+            var jsonPath = Path.Combine(Environment.GetEnvironmentVariable("LocalAppData"), infoPath);
+
+            if (!File.Exists(jsonPath)) jsonPath = Path.Combine(Environment.GetEnvironmentVariable("AppData"), infoPath);
+
+            if (!File.Exists(jsonPath)) throw new Exception("Dropbox could not be found!");
+
+            var dropboxPath = File.ReadAllText(jsonPath).Split('\"')[5].Replace(@"\\", @"\");
+            return dropboxPath;
+        }
+    }
+}
