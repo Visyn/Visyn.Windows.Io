de54afb751194e727adaf180f53d562e44124789
Visyn <visyn@randlee.com>
2017-06-04 14:52:37 -0700

    Wpf Terminal

--- Visyn.Windows.Io/Dropbox/DropboxFolder.cs
+++ Visyn.Windows.Io/Dropbox/DropboxFolder.cs
@@ -1,26 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
+
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Visyn.Windows.Io.Dropbox
 {
-    class DropboxFolder
+    public static class DropboxFolder
     {
-        public string Directory ()
+        public static string Directory ()
         {
             var infoPath = @"Dropbox\info.json";
 
-            var jsonPath = Path.Combine(Environment.GetEnvironmentVariable("LocalAppData"), infoPath);
+            var jsonPath = System.IO.Path.Combine(Environment.GetEnvironmentVariable("LocalAppData"), infoPath);
 
-            if (!File.Exists(jsonPath)) jsonPath = Path.Combine(Environment.GetEnvironmentVariable("AppData"), infoPath);
+            if (!System.IO.File.Exists(jsonPath)) jsonPath = System.IO.Path.Combine(Environment.GetEnvironmentVariable("AppData"), infoPath);
 
-            if (!File.Exists(jsonPath)) throw new Exception("Dropbox could not be found!");
+            if (!System.IO.File.Exists(jsonPath)) throw new System.IO.DirectoryNotFoundException("Dropbox directory could not be found!");
 
-            var dropboxPath = File.ReadAllText(jsonPath).Split('\"')[5].Replace(@"\\", @"\");
+            var dropboxPath = System.IO.File.ReadAllText(jsonPath).Split('\"')[5].Replace(@"\\", @"\");
+          
             return dropboxPath;
+        }
+
+        public static string Directory(string relativeDirectory)
+        {
+            var dropboxPath = DropboxFolder.Directory();
+
+            if (System.IO.Directory.Exists(dropboxPath))
+            {
+                var dropboxDirectory = System.IO.Path.Combine(dropboxPath, relativeDirectory);
+                if (System.IO.Directory.Exists(dropboxDirectory))
+                {
+                    return dropboxDirectory;
+                }
+            }
+            return dropboxPath;
+        }
+
+        public static System.IO.DirectoryInfo DirectoryInfo(string relativePath)
+        {
+            var directory = Directory(relativePath);
+           
+            return System.IO.Directory.Exists(directory) ? new  System.IO.DirectoryInfo(directory) : null;
         }
     }
 }
