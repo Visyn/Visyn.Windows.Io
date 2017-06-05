4b6a7b0932a20dd17c604c9364b000e8f8cd09f4
Visyn <visyn@randlee.com>
2017-06-05 10:11:29 -0700

    Missing/moved files

---
+++ Visyn.Windows.Io/ConsoleOutput.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+using Visyn.Core.Io;
+using Visyn.Public.Io;
+
+namespace Visyn.Windows.Io
+{
+    public class ConsoleOutput : TraceListener, IOutputDevice
+    {
+        public override void WriteLine(string line) => System.Console.WriteLine(line);
+        public override void Write(string text) => System.Console.Write(text);
+
+        #region Implementation of IOutputDevice
+
+        public void Write(Func<string> func) => System.Console.Write(func.Invoke());
+
+        #endregion
+    }
+}
