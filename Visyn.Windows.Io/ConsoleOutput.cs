9215559d961b623d10e1c709200542e7d9ae158d
randlee <github@randlee.com>
2017-08-10 13:29:18 -0700

    project clean-up.  Visyn.Public in separate repo.

--- Visyn.Windows.Io/ConsoleOutput.cs
+++ Visyn.Windows.Io/ConsoleOutput.cs
@@ -1,18 +1,17 @@
 ﻿using System;
 using System.Diagnostics;
-using Visyn.Core.Io;
 using Visyn.Public.Io;
 
 namespace Visyn.Windows.Io
 {
     public class ConsoleOutput : TraceListener, IOutputDevice
     {
-        public override void WriteLine(string line) => System.Console.WriteLine(line);
-        public override void Write(string text) => System.Console.Write(text);
+        public override void WriteLine(string line) => Console.WriteLine(line);
+        public override void Write(string text) => Console.Write(text);
 
         #region Implementation of IOutputDevice
 
-        public void Write(Func<string> func) => System.Console.Write(func.Invoke());
+        public void Write(Func<string> func) => Console.Write(func.Invoke());
 
         #endregion
     }
