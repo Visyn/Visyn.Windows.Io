395e3d96134d3b6a409ad0f9efbf443b9bbf53fe
randlee <github@randlee.com>
2017-08-10 16:46:52 -0700

    Visyn.Collection project created

---
+++ Visyn.Windows.Io/Files/DirectoryExtensions.cs
@@ -0,0 +1,27 @@
+﻿
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Visyn.Windows.Io.Files
+{
+    public static class DirectoryExtensions
+    {
+        public static IEnumerable<FileInfo> FindFiles(string directory, string fileFilter = "*.txt")
+        {
+            return Directory.Exists(directory) ? new DirectoryInfo(directory).FindFiles(fileFilter) : null;
+        }
+
+        public static IEnumerable<FileInfo> FindFiles(this DirectoryInfo directory, string fileFilter = "*.txt")
+        {
+            if (directory == null) yield break;
+
+            var files = Directory.GetFiles(directory.FullName, fileFilter);
+            foreach (var fi in directory.EnumerateFiles())
+            {
+                if (files.Contains(fi.FullName))
+                    yield return fi;
+            }
+        }
+    }
+}
--- Visyn.Windows.Io/Files/FileExtensions.cs
+++ Visyn.Windows.Io/Files/FileExtensions.cs
@@ -1,33 +1,9 @@
-﻿using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-
-namespace Visyn.Windows.Io.Files
+﻿namespace Visyn.Windows.Io.Files
 {
     public static class FileExtensions
     {
 
 
 
-    }
-
-    public static class DirectoryExtensions
-    {
-        public static IEnumerable<FileInfo> FindFiles(string directory, string fileFilter = "*.txt")
-        {
-            return Directory.Exists(directory) ? new DirectoryInfo(directory).FindFiles(fileFilter) : null;
-        }
-
-        public static IEnumerable<FileInfo> FindFiles(this DirectoryInfo directory, string fileFilter = "*.txt")
-        {
-            if (directory == null) yield break;
-
-            var files = System.IO.Directory.GetFiles(directory.FullName, fileFilter);
-            foreach (var fi in directory.EnumerateFiles())
-            {
-                if (files.Contains(fi.FullName))
-                    yield return fi;
-            }
-        }
     }
 }
